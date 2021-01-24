da56c266aa37de368edac340bd91ad7372354e69
leekichang <1kichang@yonsei.ac.kr>
2021-01-25 08:45:07 +0900

    Init Neural Network

--- Neural_Network.py
+++ Neural_Network.py
@@ -1,9 +1,25 @@
+import numpy as np
+
 class neuralNetwork:
-    def __init__():
-        pass
-    
+    def __init__(self, inputNodes, hiddenNodes, outputNodes, learningRate):
+        self.inodes = inputNodes
+        self.hnodes = hiddenNodes
+        self.onodes = outputNodes
+        self.lr = learningRate
+        self.wih = np.random.normal(0.0, pow(self.inodes, -0.5), (self.hnodes, self.inodes))
+        self.who = np.random.normal(0.0, pow(self.onodes, -0.5), (self.onodes, self.hnodes))
+
     def train():
         pass
     
     def query():
         pass
+
+inputNodes = 3
+hiddenNodes = 3
+outputNodes = 3
+
+learningRate = 0.3
+
+n = neuralNetwork(inputNodes, hiddenNodes, outputNodes, learningRate)
+print(n.wih)
